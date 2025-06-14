80272155be2b3132046f4c6c34f2896dac1424bb
unknown <kishorekummarj.cse2024@citchennai.net>
2025-06-14 22:52:05 +0530

    configured frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import toast from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+import HomePage from "./pages/HomePage";
+import CreateNotePage from "./pages/CreateNotePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div dataitheme="luxury">
+      <button onClick = { () => toast.success("Welcome") } className="btn btn-accent">Click me</button>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreateNotePage />} />
+      <Route path="/view/:id" element={<NoteDetailPage />} />
+    </Routes>
+
+    </div>
   )
 }
 
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+  return (
+    <div>noteDetailPage</div>
+  )
+}
+
+export default NoteDetailPage
